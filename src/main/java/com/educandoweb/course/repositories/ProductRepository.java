0b9ea8ec6ef980098762c0e417046bb4e62f15f6
ferrazsergio <juniorsilva6677@hotmail.com>
2024-07-30 15:02:28 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.course.entities.Product;
+
+@Repository //anotacapo opcional pois ja herda de JpaRepository que é um componente do spring
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
